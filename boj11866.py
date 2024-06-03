13275236403fc6ea39ef81a32f564e3d76797aaf
kharu_2.17 <kharu217.dev@gmail.com>
2024-06-03 16:45:24 +0900

    solve : 11866

--- boj11866.py
+++ boj11866.py
@@ -1,15 +1,24 @@
 import sys
 
+N, K = map(int, sys.stdin.readline().rstrip().split())
+
 del_addr = 0
-next_addr = 0
+next_addr = K - 1
 
-N, K = tuple(map(int, sys.stdin.readline().rstrip().split()))
-
-N_list = [p_num + 1 for p_num in range(N)]
-
+Y_list = [p_num for p_num in range(1, N + 1)]
+del_list = []
 
 
-while N_list :
-    third_n = N_list[next_addr + 2]
-    
+while Y_list :
+    del_list.append(Y_list.pop(next_addr))
+    next_addr += K - 1
 
+    while next_addr >= len(Y_list) :
+        next_addr -= len(Y_list)
+
+        if len(Y_list) == 0 :
+            break
+
+del_list = map(str, del_list)
+
+print('<' + ', '.join(del_list) + '>')
