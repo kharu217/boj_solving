c44077dc281665c685ea3c967427ec583cfb26ba
kharu_2.17 <kharu217.dev@gmail.com>
2024-06-24 16:59:05 +0900

    nothing

--- boj1920_unsolved.py
+++ boj1920_unsolved.py
@@ -1,13 +1,16 @@
-N = int(input())
-N_list = list((map(int, input().split())))
+from bisect import bisect_left
+import sys
 
+N = int(input())
+N_list = list((map(int, sys.stdin.readline().split())))
+N_list.sort()
 
 M = int(input())
-M_list = list(map(int, input().split()))
-
+M_list = list(map(int, sys.stdin.readline().split()))
+M_list.sort()
 
 for i in M_list :
-    if i in N_list :
-        print(1)
-    else :
+    if bisect_left(N_list, i) == len(N_list) + 1 :
         print(0)
+    else :
+        print(1)
