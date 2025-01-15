da4b970aa8a33be038ab317ab62e81cb6c01cf17
kharu217 <kharu217.dev@gmail.com>
2025-01-15 15:36:43 +0900

    boj11286 : solve

---
+++ boj11727.py
@@ -0,0 +1,11 @@
+N = int(input())
+l = []
+
+for i in range(1, N + 1) :
+    if i == 1 :
+        l.append(1)
+    elif i == 2 :
+        l.append(3)
+    else :
+        l.append((l[i-2] + l[i-3]) % 10007)
+print(l[-1])
