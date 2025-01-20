adbc4f86c5d405e2bcb1339972f94f9bfeb106e5
kharu217 <kharu217.dev@gmail.com>
2025-01-20 23:34:56 +0900

    boj

--- boj11727.py
+++ boj11727.py
@@ -1,11 +1,11 @@
 N = int(input())
 l = []
 
-for i in range(1, N + 1) :
-    if i == 1 :
+for i in range(1, N + 1):
+    if i == 1:
         l.append(1)
-    elif i == 2 :
+    elif i == 2:
         l.append(3)
-    else :
-        l.append((l[i-2] + l[i-3]) % 10007)
+    else:
+        l.append((l[i-2] + 2 * l[i-3]) % 10007)
 print(l[-1])
